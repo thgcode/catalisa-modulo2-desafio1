929967e4b2227679618e02c888cece6c22b2b199
Thiago Seus <thiago.seus@yahoo.com.br>
2021-02-02 13:23:32 -0300

    Adicionar código para sortear um número de 1 a 10.

--- src/com/zup/Main.java
+++ src/com/zup/Main.java
@@ -1,7 +1,11 @@
 package com.zup;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-
+        Random sorteador = new Random();
+        int numeroSorteado = sorteador.nextInt(10) + 1;
+        System.out.println(numeroSorteado);
     }
 }
