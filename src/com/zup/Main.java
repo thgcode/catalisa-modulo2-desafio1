43202480a2cd2e13c4a782b9ee90d73bd3eaf56d
Thiago Seus <thiago.seus@yahoo.com.br>
2021-02-02 14:11:49 -0300

    Adicionar pergunta se o usuário quer jogar de novo.

--- src/com/zup/Main.java
+++ src/com/zup/Main.java
@@ -13,21 +13,28 @@ public class Main {
 
         System.out.println("Jogo da sorte");
 
-        System.out.println("Qual o número (de 1 a 10)?");
-        numero = entrada.nextInt();
+        boolean continuar;
 
-        if (numero == (numeroSorteado - 1)) {
-            pontuacao += 5;
-            System.out.println("O número estava um acima do que você tentou.");
-        } else if (numero == (numeroSorteado + 1)) {
-            pontuacao += 5;
-            System.out.println("O número estava um abaixo do que você tentou.");
-        } else if (numero == numeroSorteado) {
-            pontuacao = 10;
-            System.out.println("Você acertou!");
-        } else {
-            System.out.println("Você errou. O número sorteado era " + numeroSorteado);
-        }
+        do {
+            System.out.println("Qual o número (de 1 a 10)?");
+            numero = Integer.parseInt(entrada.nextLine());
+
+            if (numero == (numeroSorteado - 1)) {
+                pontuacao += 5;
+                System.out.println("O número estava um acima do que você tentou.");
+            } else if (numero == (numeroSorteado + 1)) {
+                pontuacao += 5;
+                System.out.println("O número estava um abaixo do que você tentou.");
+            } else if (numero == numeroSorteado) {
+                pontuacao = 10;
+                System.out.println("Você acertou!");
+            } else {
+                System.out.println("Você errou. O número sorteado era " + numeroSorteado);
+            }
+
+            System.out.println("Quer jogar de novo? Digite S ou N:");
+            continuar = entrada.nextLine().equalsIgnoreCase("s");
+        } while (continuar);
 
         System.out.println("Você obteve " + pontuacao + " pontos.");
     }
