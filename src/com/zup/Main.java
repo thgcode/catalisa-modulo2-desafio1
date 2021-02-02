0777d0e1bf993068272d8739e31b0eda9dd69122
Thiago Seus <thiago.seus@yahoo.com.br>
2021-02-02 14:01:27 -0300

    Alterar para ficar de acordo com a descrição do exercício.

--- src/com/zup/Main.java
+++ src/com/zup/Main.java
@@ -7,27 +7,19 @@ public class Main {
     public static void main(String[] args) {
         Random sorteador = new Random();
         int numeroSorteado = sorteador.nextInt(10) + 1;
-        Scanner entrada = new Scanner(System.in);
-        boolean continua;
         int numero;
+        Scanner entrada = new Scanner(System.in);
 
         System.out.println("Jogo da sorte");
 
-        do {
-            System.out.println("Qual o número (de 1 a 10)?");
-            numero = entrada.nextInt();
-            continua = (numero == (numeroSorteado - 1)) || (numero == (numeroSorteado + 1));
+        System.out.println("Qual o número (de 1 a 10)?");
+        numero = entrada.nextInt();
 
-            if (continua) {
-                if (numeroSorteado < numero) {
-                    System.out.println("O número é menor.");
-                } else {
-                    System.out.println("O número é maior.");
-                }
-            }
-        } while (continua);
-
-        if (numero == numeroSorteado) {
+        if (numero == (numeroSorteado - 1)) {
+            System.out.println("O número é menor.");
+        } else if (numero == (numeroSorteado + 1)) {
+            System.out.println("O número é maior.");
+        } else if (numero == numeroSorteado) {
             System.out.println("Você acertou!");
         } else {
             System.out.println("Você errou. O número sorteado era " + numeroSorteado);
